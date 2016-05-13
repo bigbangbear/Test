ca0849a51b5276e8e04073c3161b437128885580
yuhuibin123 <385169523@qq.com>
2016-05-13 11:37:39 +0800

    添加注释

--- ugallery/src/main/java/vstore/netease/com/ugallery/UGallery.java
+++ ugallery/src/main/java/vstore/netease/com/ugallery/UGallery.java
@@ -10,48 +10,74 @@ import vstore.netease.com.ugallery.activity.ActivitySelectImage;
 import vstore.netease.com.ugallery.activity.ActivityTakePhotos;
 
 /**
+ * 发起Gallery后，结果在 onActivityResult() 中返回，调用{@link UGallery#getData(Intent)}获得Uri
  * @author yuhuibin
  * @date 2016-04-28
  */
 public class UGallery {
     public static final int TAKE_PHOTO = 1001;
     public static final int SELECT_PHOTO = 1101;
-    //public static int SELECT_MUTIL_PHOTO = 1102;
+    public static final int SELECT_MUTIL_PHOTO = 1102;
     public static final int CROP_IMAGE = 1200;
 
     public static String PATH = "PATH";
 
+    /**
+     * 选择单张图像
+     * @param context
+     */
     public static void selectSingleImage(Context context ) {
         ActivitySelectImage.startActivityForSingleImage(context, false);
     }
 
+    /**
+     * 选择单张图像并裁剪
+     * @param context
+     */
     public static void selectSingleImageCrop(Context context ) {
         ActivitySelectImage.startActivityForSingleImage(context, true);
     }
 
+    /**
+     * 选择多张图像
+     * @param context
+     */
     public static void selectMutipleImage(Context context ) {
         ActivitySelectImage.startActivityForMutilImage(context);
     }
 
+    /**
+     * 裁剪图像
+     * @param context
+     * @param uri
+     */
     public static void cropImage(Context context, Uri uri) {
         ActivityCropImage.startActivity(context, uri);
     }
 
+    /**
+     * 拍照
+     * @param context
+     */
     public static void takePhoto(Context context) {
         ActivityTakePhotos.startActivityForTakePhoto(context, false );
     }
 
+    /**
+     * 拍照并裁剪
+     * @param context
+     */
     public static void takePhotoCrop(Context context) {
         ActivityTakePhotos.startActivityForTakePhoto(context, true );
     }
 
     /**
-     * onActivityResult中返回结果，从intent(data)中解析数据
-     * @param intent
+     * 对于onActivityResult中返回结果，从intent(data)中解析数据
+     * @param data
      * @return
      */
-    public static Uri getData(Intent intent){
-        String path = intent.getStringExtra(PATH);
+    public static Uri getData(Intent data){
+        String path = data.getStringExtra(PATH);
         Uri uri = Uri.parse("file://"+path);
         return uri;
     }
